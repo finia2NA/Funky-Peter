b5ee493d3d7d01fc036168bb0f9039ab1e37a942
Christoph Fricke <christoph@frickeonline.de>
2019-02-25 11:09:34 +0100

    Add Pretty

--- src/Pretty.hs
+++ src/Pretty.hs
@@ -1,12 +1,21 @@
--- I am so pretty...
--- I'm sexy and I know it
+-- I am so pretty!
+module (Pretty(..)) where
 
 import Term
 
--- class Pretty where
-pretty :: Term -> String
-pretty (Var a) = a
-pretty (Comb name []) = name
-pretty (Comb name xs) = "(" ++ name ++ " " ++ (foldr helper "" xs) ++ ")"
-  where
-  helper term oldstring = oldstring ++ (pretty term)
+class Pretty where
+  pretty :: Term -> String
+  pretty (Var a) = a
+  pretty (Comb name []) = name
+  pretty (Comb name xs) = name ++ "" ++ (foldl helper "" xs)
+    where
+    helper oldstring term = oldstring ++ " " ++ (pretty' term)
+    pretty' x@(Var a) = pretty x
+    pretty' x@(Comb name []) = pretty x
+    pretty' (Comb name xs) = "(" ++ name ++ "" ++ (foldl helper "" xs) ++ ")"
+  
+-- -- I'm pretty too
+-- pretty2 :: Term -> String
+-- pretty2 (Var a) = a
+-- pretty2 (Comb name []) = name
+-- pretty2 (Comb name [x : []]) = 
