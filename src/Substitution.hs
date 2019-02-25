2be101027baae3ee5eb21d8050929e856cbf4cc3
Christoph Fricke <christoph@frickeonline.de>
2019-02-25 12:04:08 +0100

    Substitution Jutsu!

--- src/Substitution.hs
+++ src/Substitution.hs
@@ -1,1 +1,16 @@
 -- ADT for subtitution from variables to terms
+
+import Term
+
+data Subst = Sub (VarName -> Term)
+
+identity :: Subst
+identity = Sub (\x -> Var x)
+
+single:: VarName -> Term -> Subst
+single v term = Sub (\x -> term)
+
+compose:: Subst -> Subst -> Subst
+compose s1 s2 = Sub (s1 . s2)
+
+apply:: Subst -> Term -> Term
