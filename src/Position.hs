6f5c241721fea33d66053c60fe2476febf9918bb
Christoph Fricke <christoph@frickeonline.de>
2019-02-26 12:00:27 +0100

    Add all function for task 3. Their usefulness is weird to me...

--- src/Position.hs
+++ src/Position.hs
@@ -9,3 +9,20 @@ type Pos = Int
 -- What should happen if n is greater than the var count?
 selectAt:: Term -> Pos -> Term
 selectAt (Comb _ xs) n = xs !! n
+
+-- I am assuming that the position means the index in a subterm at a depth of 0.
+-- Meaning we do not dive recursivly into sub sub terms.
+-- This makes the most sence for me considering that we want to use this functionality
+-- when evaluting terms.
+replaceAt:: Term -> Pos -> Term -> Term
+replaceAt t@(Comb _ []) _ _  = t
+replaceAt (Comb name xs) n term = Comb name (replaceSub xs n term)
+  where
+    replaceSub:: [Term] -> Pos -> Term -> [Term]
+    replaceSub [] _ _ = []
+    replaceSub (_:xs) 0 term = term : xs
+    replaceSub (x:xs) n term = x : (replaceSub xs (n-1) term)
+
+-- ?!?! Nani. Behaviour looks a bit strange to me... This is fucking stupid :(
+allPos:: Term -> [Pos]
+allPos (Comb _ xs) = take (length xs) [0..]
