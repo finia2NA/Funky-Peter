79a2064436f1aa03395528e9dbe3ff92ca035e70
Finite <33845166+finia2NA@users.noreply.github.com>
2019-02-27 18:41:34 +0100

    if this would work it would be optimal
    but it doesnt so next commit is gonna be a restriction that should work

--- src/Substtest.hs
+++ src/Substtest.hs
@@ -1,15 +1,18 @@
 import Test.QuickCheck
 import Subst
+import Term
 
 instance Arbitrary Term where
   arbitrary =
     sized asTerm
-    where
+     where
       asTerm :: Int -> Gen (Term)
       asTerm s
-      | s == 0 = do
-        name = arbtirary
-        return (Var name)
-      | otherwise = do
-        listLength <- choose (0, s - 1)
-        treeList
+        | s == 0 = do
+          name <- arbitrary
+          return (Var name)
+        | otherwise = do
+          name <- arbitrary
+          listLength <- choose (0, s - 1)
+          termList <- [term | tlength <- choose(0, s-1), term <- asTerm tlength]
+          return (Comb name termList)
