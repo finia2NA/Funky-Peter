a6cb849ca3b8ab12a7a0e63d457e8d87256bf178
Finite <33845166+finia2NA@users.noreply.github.com>
2019-02-27 19:12:39 +0100

    change Subst Constructor

--- src/Subst.hs
+++ src/Subst.hs
@@ -2,18 +2,18 @@ module Subst where
 
 import Term
 
-data Subst = Sub (Term -> Term)
+data Subst = Subst (Term -> Term)
 
 identity :: Subst
-identity = Sub (\x -> x)
+identity = Subst (\x -> x)
 
 single:: VarName -> Term -> Subst
-single v term = Sub (\x -> if x == (Var v) then term else x)
+single v term = Subst (\x -> if x == (Var v) then term else x)
 
 compose:: Subst -> Subst -> Subst
-compose (Sub f1) (Sub f2) = Sub (f1 . f2)
+compose (Subst f1) (Subst f2) = Subst (f1 . f2)
 
 apply:: Subst -> Term -> Term
-apply (Sub sub) term = sub term
+apply (Subst sub) term = sub term
 
 test1 = apply (single ("hi") (Var "ho")) (Var "hi")
