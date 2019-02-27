5161936290b0b91a42747318844f439fd2d2f231
Finite <33845166+finia2NA@users.noreply.github.com>
2019-02-27 13:29:22 +0100

    Comments

--- src/Position.hs
+++ src/Position.hs
@@ -10,23 +10,26 @@ above pos1 pos2 = length pos1 < length pos2 && pos1 == (take (length pos1) pos2)
 below :: Pos -> Pos -> Bool
 below pos1 pos2 = length pos2 < length pos1 && pos2 == (take (length pos2) pos1)
 
--- Returns true if the first pos is left in the same term tree of pos2
+-- is pos1 left of pos2?
 leftOf :: Pos -> Pos -> Bool
 leftOf pos1 pos2  = (init pos1 == init pos2) && (last pos1) < (last pos2)
   
--- Returns true if the first pos is right in the same term tree of pos2
+-- is pos1 right of pos2?
 rightOf :: Pos -> Pos -> Bool
 rightOf pos1 pos2 = (init pos1 == init pos2) && (last pos1) > (last pos2)
 
+-- returns the subterm at a given position
 selectAt :: Term -> Pos -> Term
 selectAt term [] = term
 selectAt (Comb _ xs) (p:ps) = selectAt (xs !! p) ps
 
+-- replaces switches out a subterm at a given position.
 replaceAt :: Term -> Pos -> Term -> Term
 replaceAt src [] rpl = rpl
 replaceAt (Comb n ss) (p:ps) rpl = let (x, (y: ys)) = splitAt p ss in
   Comb n (x ++ [(replaceAt y ps rpl)] ++ ys)
 
+-- returns a list of all Possible Positions within the Term.
 allPos :: Term -> [Pos]
 allPos (Var v) = [[]]
 allPos (Comb c xs) = let li = (length xs) - 1 in -- li = lastIndex
