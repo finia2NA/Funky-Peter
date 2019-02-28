ee6fe249b7d2dab1d6ea07c936b617f31135eee8
Christoph Fricke <christoph@frickeonline.de>
2019-02-28 12:00:46 +0100

    Add functioning shit

--- src/Reduction.hs
+++ src/Reduction.hs
@@ -2,42 +2,27 @@ import Matching
 import Prog
 import Term
 import Subst
+import Data.List
 
 -- Suche Regel in Prog (Liste von Regeln) für die gilt:
 --  - Linke Seite ist == Term
 --  - Es existiert match zwischen term und rhs
-      
--- findRule1 :: Prog -> Term -> Maybe(Rhs, Subst)
--- findRule1 prog term =
---   myReturn term (myFinder prog term)
---  where 
---   myReturn _ Nothing = Nothing
---   myReturn term (Just Rhs) = Just(Rhs, Subst.single(term, Rhs))
 
---   myFinder [] _ = Nothing
---   myFinder ((Rule Lhs Rhs):s) term
---     | unifiable term Lhs = Just Rhs
---     | otherwise = myFinder s term
-
---   unifiable specific general
---     | Matching.match general specific == Nothing = False
---     | otherwise = True
-
-      
-findRule2 :: Prog -> Term -> Maybe(Rhs, Subst)
-findRule2 prog term = unwrap find filter (map mapper prog)
+findRule:: Prog -> Term -> Maybe(Rhs, Subst)
+findRule (Prog rules) term = foldr reductor Nothing rules
  where
-  mapper (Rule lh rh ) = let susbst = maybe rh term in 
-    if match lf term != Nothing && subst != Nothing
-      then Just (rh, subst)
-      else Nothing
-  filter (Just _ _) = True
-  filter _ = False
-  unwrap :: Maybe a -> a
-  unwrap (Just x) = x
-  unwrap (Nothing) = Nothing
+  reductor (Rule lh rh) acc =
+    if maybeCompare (match lh term) && maybeCompare(match rh term)
+      then if maybeCompare acc then acc else Just (rh, (unwrap (match rh term)))
+      else if maybeCompare acc then acc else Nothing
+  maybeCompare (Just _) = True
+  maybeCompare _        = False
 
 
-testRules = [(Rule (Comb "add" [Comb "ZERO" [], Var "m"]) (Var "m"))]
+unwrap :: Maybe a -> a
+unwrap (Just a) = a
+
+
+testRules = Prog [(Rule (Comb "add" [Comb "ZERO" [], Var "m"]) (Var "m"))]
 testTerm = (Comb "add" [Comb "ZERO" [], Comb "SUCC" [Comb "SUCC" [Comb "ZERO" []]]])
         
