d7823a26f7c5720574ffdf54f2884df236edea24
Finite <33845166+finia2NA@users.noreply.github.com>
2019-02-28 21:42:57 +0100

    unnötiges zeug löschen

--- src/Pretty.hs
+++ src/Pretty.hs
@@ -10,10 +10,10 @@ class Pretty a where
 instance Pretty Term where
   pretty (Var a) = a
   pretty (Comb name []) = name
-  pretty (Comb name xs) = name ++ "" ++ (foldl helper "" xs)
+  pretty (Comb name xs) = name ++ (foldl helper "" xs)
    where
     helper oldstring term = oldstring ++ " " ++ (pretty' term)
     pretty' x@(Var a) = pretty x
     pretty' x@(Comb name []) = pretty x
     -- Put brackets arround inner function calls
-    pretty' (Comb name xs) = "(" ++ name ++ "" ++ (foldl helper "" xs) ++ ")"
+    pretty' (Comb name xs) = "(" ++ name ++ (foldl helper "" xs) ++ ")"
