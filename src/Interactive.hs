02c5da40b649e68a669f22ee179dd921d9f1c08a
Christoph Fricke <christoph@frickeonline.de>
2019-03-01 17:28:20 +0100

    Add "Hello World"

--- src/Interactive.hs
+++ src/Interactive.hs
@@ -2,6 +2,17 @@ import Parser
 
 -- main :: IO ()
 
-getFile :: Parse a => String -> IO (Either String a)
-getFile url = parseFile url
+-- data State s p= State (Strategy s) (Program p)
 
+-- programmLoop :: IO State -> IO State
+-- getFile :: Parse a => String -> IO (Either String a)
+-- getFile url = parseFile url
+
+printWelcome :: IO ()
+printWelcome = do
+  print "Welcome to Simple Haskell!"
+  print "Type \":h(elp)\" for help."
+
+main :: IO ()
+main = do
+  printWelcome 
