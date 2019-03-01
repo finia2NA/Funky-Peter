6104a80e3bb4b515e15ea2c8361c5188aded4089
Christoph Fricke <christoph@frickeonline.de>
2019-03-01 14:45:26 +0100

    Wall

--- src/Matching.hs
+++ src/Matching.hs
@@ -22,7 +22,7 @@ match (Comb x xs) (Comb y ys) =
   else Nothing
    where
     composeMaybeSubst :: Maybe Subst -> Maybe Subst -> Maybe Subst
-    composeMaybeSubst Nothing      acc        = Nothing
+    composeMaybeSubst Nothing      _        = Nothing
     composeMaybeSubst (Just subst) (Just acc) = Just (Subst.compose subst acc)
     composeMaybeSubst _            Nothing    = Nothing
 
@@ -32,20 +32,21 @@ matchV t1 t2
 -- wenn es für jede Var ein subst geben wird,
 -- dann return ein composed Subst von all diesen.
   | (etav t1 t2) && (ace t1 t2) = Just (
-  foldr Subst.compose Subst.identity (map (getSubst t1 t2) (findAllVars t1))
+  foldr Subst.compose Subst.identity (map (getSubst) (findAllVars t1))
   )
   | otherwise = Nothing
   where
     -- gets the Substitution from t1.pos (which is a var) to t2.pos
-  getSubst :: Term -> Term -> Pos -> Subst
-  getSubst t1 t2 matchedPosition =
+  getSubst ::  Pos -> Subst
+  getSubst matchedPosition =
     let grabVar = (selectAt t1 matchedPosition)
         grabTerm = (selectAt t2 matchedPosition)
-    in helper grabVar grabTerm
+    in getSubst' grabVar grabTerm
     where
       -- construct a Subst from a Variable and a Term.
-      helper :: Term -> Term -> Subst
-      helper (Var varname) term = single varname term
+      getSubst' :: Term -> Term -> Subst
+      getSubst' (Var varname) term = single varname term
+      getSubst' (Comb _ _) _ = error "no"
 
 ace :: Term -> Term -> Bool
 ace t1 t2  = let allCombs = (allPos t1) \\ (findAllVars t1) in
@@ -53,16 +54,10 @@ ace t1 t2  = let allCombs = (allPos t1) \\ (findAllVars t1) in
   where
     helper bool pos = bool && (sameStructure (selectAt t1 pos) (selectAt t2 pos))
     sameStructure (Comb n1 l1) (Comb n2 l2) = n1==n2 && (length l1)==(length l2)
+    sameStructure (Var _) (Comb _ _) = error "fuck you"
+    sameStructure (Comb _ _) (Var _) = error "fuck you"
+    sameStructure x@(Var _) y@(Var _) = x==y
 
-{-
-  given two terms t1 and t2,
-  is there a term in t2 at the same position as every var in t1?
-  
-  AKA gilt 
-  ∀var∈t1: ∃term∈t2: t1.pos=t2.pos
-  ?
--}
--- (etav stands for EXISTS TERM at ALL VARIABLES btw)
 etav :: Term -> Term -> Bool
 etav t1 t2 = let baum = (Pos.allPos t2) in
   foldl (helper baum) True (findAllVars t1)
@@ -91,11 +86,11 @@ findAllVars t = filter (\p -> isVar (Pos.selectAt t p)) (Pos.allPos t)
 -- test3 = apply (unwrap (match testvar3 testvar4)) testvar3
 
 
-testT1 = Comb "add" [(Comb "ONE" []), (Comb "ONE" [])]
-testT2 = Comb "mult" [(Comb "ONE" []), (Comb "ONE" [])]
+-- testT1 = Comb "add" [(Comb "ONE" []), (Comb "ONE" [])]
+-- testT2 = Comb "mult" [(Comb "ONE" []), (Comb "ONE" [])]
 
-testT3 = Comb "SUCC" [Comb "SUCC" [Comb "SUCC" [Var "m"]]]
-testT4 = Comb "SUCC" [Comb "SU" [Comb "SUCC" [Comb "ZERO" []]]]
+-- testT3 = Comb "SUCC" [Comb "SUCC" [Comb "SUCC" [Var "m"]]]
+-- testT4 = Comb "SUCC" [Comb "SU" [Comb "SUCC" [Comb "ZERO" []]]]
 
-testV1 = Comb "hi" [(Var "x")]
-testV2 = Comb "ho" [(Comb "y" [])]
+-- testV1 = Comb "hi" [(Var "x")]
+-- testV2 = Comb "ho" [(Comb "y" [])]
