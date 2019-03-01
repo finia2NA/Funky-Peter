3761198a62e46a1fb39f992ef52bd769d1448cfe
Christoph Fricke <christoph@frickeonline.de>
2019-03-01 13:51:11 +0100

    Add the perfect match

--- src/Matching.hs
+++ src/Matching.hs
@@ -10,63 +10,20 @@ import Pos
 -- 2.5. Bekomme term in t2 an der pos ✔
 -- 3. ersetze diese var durch den entsprechenden austruck in t2 ✔
 
--- Returns a list of substitutions from t1 to t2 if it is possible
 match :: Term -> Term -> Maybe Subst
-match t1 t2 
--- wenn es für jede Var ein subst geben wird,
--- dann return ein composed Subst von all diesen.
-  | etav t1 t2 = Just (
-  foldr Subst.compose Subst.identity (map (getSubst t1 t2) (findAllVars t1))
-  )
-  | otherwise = Nothing
-  where
-    -- gets the Substitution from t1.pos (which is a var) to t2.pos
-  getSubst :: Term -> Term -> Pos -> Subst
-  getSubst t1 t2 matchedPosition =
-    let grabVar = (selectAt t1 matchedPosition)
-        grabTerm = (selectAt t2 matchedPosition)
-    in helper grabVar grabTerm
-    where
-      -- construct a Subst from a Variable and a Term.
-      helper :: Term -> Term -> Subst
-      helper (Var varname) term = single varname term
-
-
--- Returns a list of substitutions from t1 to t2 if it is possible
-match2 :: Term -> Term -> Maybe Subst
-match2 t1 t2 
-  | etav t1 t2 = Just (
-      foldr Subst.compose Subst.identity (
-        map (\pos ->
-          (\(Var v) -> Subst.single v (Pos.selectAt t2 pos))
-          $ (Pos.selectAt t1 pos)
-          )
-          (findAllVars t1)
-      )
-    )
-  | otherwise = Nothing
-
-      {-
-      given two terms t1 and t2,
-  is there a term in t2 at the same position as every var in t1?
-  
-  AKA gilt 
-  ∀var∈t1: ∃term∈t2: t1.pos=t2.pos
-  ?
-  -}
--- (etav stands for EXISTS TERM at ALL VARIABLES btw)
-etav :: Term -> Term -> Bool
-etav t1 t2 = let tree = (Pos.allPos t2) in
-  foldl (existsPath tree) True (findAllVars t1)
+match (Comb _ _)  (Var _)     = Nothing
+match (Var v)     t           = Just (Subst.single v t)
+match (Comb x xs) (Comb y ys) =
+  if x == y && length xs == length ys
+  -- custom for loop || map mapped to [Mabye Subst]
+  then foldr composeMaybeSubst (Just Subst.identity)
+    (map (\i -> match (xs !! i) (ys !! i)) [0 .. (length xs - 1)])
+  else Nothing
    where
-    existsPath tree' prevBool path  = prevBool && (elem path tree')
-
--- Returns the positions of all vars in a given term
-findAllVars :: Term -> [Pos]
-findAllVars t = filter (\p -> isVar (Pos.selectAt t p)) (Pos.allPos t)
- where
-  isVar (Var _) = True
-  isVar _ = False
+    composeMaybeSubst :: Maybe Subst -> Maybe Subst -> Maybe Subst
+    composeMaybeSubst Nothing      acc        = Nothing
+    composeMaybeSubst (Just subst) (Just acc) = Just (Subst.compose subst acc)
+    composeMaybeSubst _            Nothing    = Nothing
 
 --------------------- test code ---------------------
 
@@ -87,7 +44,7 @@ testT1 = Comb "add" [(Comb "ONE" []), (Comb "ONE" [])]
 testT2 = Comb "mult" [(Comb "ONE" []), (Comb "ONE" [])]
 
 testT3 = Comb "SUCC" [Comb "SUCC" [Comb "SUCC" [Var "m"]]]
-testT4 = Comb "SUCC" [Comb "SUC" [Comb "SUCC" [Comb "ZERO" []]]]
+testT4 = Comb "SUCC" [Comb "SU" [Comb "SUCC" [Comb "ZERO" []]]]
 
 testV1 = Comb "SUCC" [(Comb "a" []), (Var "x")]
 testV2 = Comb "PRE" [(Comb "a" []), (Comb "y" [])]
