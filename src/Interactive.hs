d07f2d621bdcae53759e9484d6ccf89f1053c7c9
Christoph Fricke <christoph@frickeonline.de>
2019-03-05 14:50:18 +0100

    Add oppertunity to reload files that failed parsing

--- src/Interactive.hs
+++ src/Interactive.hs
@@ -93,7 +93,8 @@ loadFile state cmd = do
       let filePath = args !! 1
       eitherProg <- Parser.parseFile filePath
       case eitherProg of
-        (Left msg) -> putStrLn msg >> return (Just state)
+        (Left msg) -> putStrLn msg >> 
+          return (Just (State.setProgram state "" (Prog []) (Just filePath)))
         (Right prog) -> putStrLn ("File loaded.") >>
           return (Just (State.setProgram state 
             (System.FilePath.Posix.takeBaseName filePath) prog (Just filePath)))
