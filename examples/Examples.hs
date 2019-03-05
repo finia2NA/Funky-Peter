473da9289ca9e3edc49ee6f2f88bd14e7eb1c618
Finite <33845166+finia2NA@users.noreply.github.com>
2019-03-05 17:33:15 +0100

    Update Examples.hs

--- examples/Examples.hs
+++ examples/Examples.hs
@@ -1,43 +1,45 @@
 module Example where
   -- 8.1, 5
+  -- Regeln für 8.1, 5:
   f :: Peano -> Peano
   f x = Zero
   
   h :: Peano
   h = h
   
-  -- wenn man (f h) in einem call-by-value modus ausführt, bekommt man eine
-  -- unendliche rekursion. bei call-by-name eval bekommt man das ergebniss Zero.
+-- | 8.1:
+--   wenn man (f h) in einem call-by-value modus ausführt, bekommt man eine
+--   unendliche rekursion. bei call-by-name eval bekommt man das ergebniss Zero.
 exp1 = f h
 
--- da die funktion h aus 8.1 sich konstant selbst aufruft und nice tatsächelich
--- eine peano-zahl returnt, terminiert sie nie.
+-- | 8.5:
+--   da die funktion h aus 8.1 sich konstant selbst aufruft und nice tatsächlich
+--   eine peano-zahl returnt, terminiert sie nie.
 exp5 = h
 
+
+
 -- 8.2, 3
+
+-- daten für 8.2, 3
 data Boolean = True | False
 
-genTrue = True
+
+-- Regeln für 8.2, 3
+f2 _ True = True
+f2 _ False = False
 
 f3 True _ = True
 f3 False _ = False
 
-f2 _ True = True
-f2 _ False = False
+genTrue = True
 
 endlessBool :: Bool
 endlessBool = endlessBool
 
+
+-- | Lösung für 8.2: Terminiert für RO, nicht für LO
 exp2 = f2 endlessBool genTrue
+
+-- | Lösung für 8.3: Terminiert für LO, nicht für RO
 exp3 = f3 genTrue endlessBool
-
-
--- 8.4
-t :: Boolean -> Boolean
-t True = True
-t False = t False
-
-comp :: Boolean -> Boolean -> Boolean
-comp t1 x = (t1 x)
-
-exp4 = comp t genTrue
