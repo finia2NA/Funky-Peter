19db3e177184a835fa71248563fef05739d322a2
Finite <33845166+finia2NA@users.noreply.github.com>
2019-03-18 23:20:20 +0100

    Haddock lmao

--- src/Util.hs
+++ src/Util.hs
@@ -1,11 +1,11 @@
 module Util where
 
--- helper function for Maybe: returns whether the argument is a Just or Nothing
+-- | helper function for Maybe: returns whether the argument is a Just or Nothing
 notNothing :: Maybe a -> Bool
 notNothing (Just _) = True
 notNothing _        = False
 
--- Unwraps the Data from a Maybe container. Will throw an exception on Nothing.
+-- | Unwraps the Data from a Maybe container. Will throw an exception on Nothing.
 unwrap :: Maybe a -> a
 unwrap (Just a) = a 
 unwrap Nothing = error "Tried to unwrap a Nothing instance of Maybe type."
